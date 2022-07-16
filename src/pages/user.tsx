6b2f37be0a8e85cb6ff998613aad9b04caee8c40
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-07-16 15:56:38 +0900

    create UserIcon

---
+++ src/pages/user.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { UserIcon } from "../components/atoms";
+
+const user = () => {
+  return (
+    <div className="flex justify-around">
+      <div>user page</div>
+      <UserIcon />
+    </div>
+  );
+};
+
+export default user;
