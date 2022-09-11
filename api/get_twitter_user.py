5396eeb84cf85a95643325f890387749f6d4e31b
hiroaki gotoh <gotoh-hiroaki-yw@ynu.ac.jp>
2022-09-12 06:49:15 +0900

    cursorを使ったものに変更

--- api/get_twitter_user.py
+++ api/get_twitter_user.py
@@ -6,7 +6,7 @@ import tweepy
 from os import getenv
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
-import pandas as pd
+import time
 
 load_dotenv()
 # 同じフォルダに.envファイルを入れておくと読み込んでくれる。
@@ -23,7 +23,8 @@ api = tweepy.API(auth, wait_on_rate_limit = True)
 
 # idからfollowerを取得
 def follower_get(screen_name):
-    followers = api.get_followers(screen_name = screen_name)
+    # itemsの()の中の数字を変更すると取得できるfollowerの数が変えられる。
+    followers = tweepy.Cursor(api.get_followers, screen_name=screen_name, cursor=-1).items(20)
     return followers
 
 def get_follower_name_list(followers):
@@ -35,7 +36,7 @@ def get_follower_name_list(followers):
 def get_follower_screen_name_list(followers):
     follower_screen_names = []
     for follower in followers:
-        follower_screen_names.append(follower.screen_name)
+        follower_screen_names.append(follower.name)
     return follower_screen_names
 
 def get_follower_description_list(followers):
@@ -68,7 +69,7 @@ def get_all_information(screen_name):
 
 if __name__ == "__main__":
     driver = GraphDatabase.driver("bolt://localhost:57687", auth=("neo4j", "password"))
-
+    start = time.time()
     # ここから先を検討中
     def add_friend(tx, name, friend_name):
         tx.run("MERGE (a:Person {name: $name}) "
@@ -78,34 +79,39 @@ if __name__ == "__main__":
     screen_name = "shimabu_it"
 
     with driver.session() as session:
-        name = []
+        names = []
         try:
             followers = follower_get(screen_name)
+            print(followers)
             names = get_follower_screen_name_list(followers)
+            print(names)
         except:
-            print("error"+ screen_name)
+            print("error_"+ screen_name)
 
         for follower_name in names:
             names2 = []
 
             try:
                 session.write_transaction(add_friend, screen_name, follower_name)
-                # print(follower_name)
+                print(follower_name)
                 follower_followers = follower_get(follower_name)
                 names2 = get_follower_screen_name_list(follower_followers)
-                # print(names2)
+                print(names2)
             except:
-                # print("error" + follower_name)
+                print("error_" + follower_name)
 
             # followerのfollowerをとって、登録
 
             for follower_follower_name in names2:
+
                 try:
                     session.write_transaction(add_friend, follower_name, follower_follower_name)
-                    # print(follower_follower_name)
+                    print(follower_follower_name)
                 except:
-                    print("error"+ follower_follower_name)
+                    print("error_"+ follower_follower_name)
 
     #
+    end = time.time()
+    print(end - start)
 
     driver.close()
